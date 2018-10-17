368526fb1cc70ab5fa990db55a26b97726e20dac
Kim Minyeob <stankim0525@gmail.com>
2018-10-17 16:12:55 +0900

    wansung

--- src/index.js
+++ src/index.js
@@ -1,5 +1,102 @@
-import './index.scss';
+let boardState = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
+function drawBorad() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add("checked");
+      } else {
+        colEl.classList.remove("checked");
+      }
+    });
+  });
+  if (bingo(boardState)) {
+    document.querySelector(".result").textContent = "BINGO!";
+    document.querySelector(".reset").classList.add("show");
+  } else {
+    document.querySelector(".result").textContent = "";
+    document.querySelector(".reset").classList.remove("show");
+  }
+}
+
+drawBorad();
+
+function bingo(arr) {
+  for (let i = 0; i < 5; i++) {
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][i] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][4 - j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+    colEl.addEventListener("click", e => {
+      if (!bingo(boardState)) {
+        boardState[rowIndex][colIndex] = 1;
+        drawBorad();
+      }
+    });
+  });
+});
+
+document.querySelector(".reset").addEventListener("click", e => {
+  boardState = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  drawBorad();
 });
